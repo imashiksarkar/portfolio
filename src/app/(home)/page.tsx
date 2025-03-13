e09b1f2f7a2e2c26edabb699ee30f8b7a855593f
Ashik Sarkar <ashiksarkar.s18@gmail.com>
2025-03-13 13:11:04 +0600

    -fix. navbar

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -5,7 +5,7 @@ import Navbar from '@/components/Navbar'
 export default function Home() {
   return (
     <section className='home-page w-full min-h-dvh nax-h-full grid grid-flow-row grid-rows-[auto_1fr_auto] grid-cols-1'>
-      <header className='row-[1/2] col-[1/2] py-5 z-20'>
+      <header className='row-[1/2] col-[1/2] md:py-5 z-20'>
         <Navbar />
       </header>
       <main className='row-[1/3] col-[1/2]'>
--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,10 +1,17 @@
+'use client'
+
 import Image from 'next/image'
 import Link from 'next/link'
 import CtaButton from '@/components/CtaButton'
+import { GiHamburgerMenu } from 'react-icons/gi'
+import { useState } from 'react'
+import { RxCrossCircled } from 'react-icons/rx'
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false)
+
   return (
-    <nav className='con flex items-center justify-between py-2.5 bg-slate-950/30 backdrop-blur-xs rounded-md text-white font-medium text-2xl'>
+    <nav className='con flex items-center justify-between py-2.5 bg-slate-950/30 backdrop-blur-xs md:rounded-md text-white font-medium text-2xl h-full'>
       <Link href='/'>
         <Image
           src='/logo-200x200-compressed.png'
@@ -15,7 +22,7 @@ const Navbar = () => {
         />
       </Link>
 
-      <ul className='flex gap-12 capitalize items-center'>
+      <ul className='gap-12 capitalize items-center hidden md:flex'>
         <li>
           <Link href='/'>home</Link>
         </li>
@@ -32,6 +39,46 @@ const Navbar = () => {
           <CtaButton href='/#contact'>contact me</CtaButton>
         </li>
       </ul>
+
+      <div
+        className={`h-dvh w-full bg-slate-950 fixed top-0 z-50 py-4 md:hidden transition-all duration-300 ${
+          isOpen ? 'right-0' : '-right-[100%]'
+        }`}
+        onClick={() => setIsOpen(false)}
+      >
+        <div className='con flex flex-col gap-8'>
+          <button
+            className='self-end cursor-pointer'
+            onClick={() => setIsOpen(false)}
+          >
+            <RxCrossCircled className='text-4xl' />
+          </button>
+          <ul className='flex flex-col gap-6 capitalize items-center'>
+            <li>
+              <Link href='/'>home</Link>
+            </li>
+            <li>
+              <Link href='/#about'>about</Link>
+            </li>
+            <li>
+              <Link href='/#skills'>skills</Link>
+            </li>
+            <li>
+              <Link href='/#education'>education</Link>
+            </li>
+            <li>
+              <CtaButton href='/#contact'>contact me</CtaButton>
+            </li>
+          </ul>
+        </div>
+      </div>
+
+      <button
+        className='bg-orange-400 md:hidden h-10 p-1 aspect-square cursor-pointer'
+        onClick={() => setIsOpen(true)}
+      >
+        <GiHamburgerMenu className='h-full w-full' />
+      </button>
     </nav>
   )
 }
